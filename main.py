65dabb00c7d77dc8ef3c182121192f5121bbe308
NathanRMS1 <nathanconta01@gmail.com>
2023-11-10 20:42:35 -0300

    Tudo em portugues

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class MainApp(MDApp):
 ScreenManager:
     Main:
 <Main>:
-    name : 'Calculator App'
+    name : 'SuperCalc'
 
     BoxLayout:
         orientation: "vertical"
@@ -42,20 +42,20 @@ ScreenManager:
                     MDTextField:
                         id: val1
                         input_filter: 'float'
-                        hint_text: "Enter the first value"
+                        hint_text: "Insira o primeiro valor"
                         color_mode: 'custom'
                         helper_text_mode: "on_focus"
 
                     MDTextField:
                         id: val2
                         input_filter: 'float'
-                        hint_text: "Enter the second value"
+                        hint_text: "Insira o segundo valor"
                         color_mode: 'custom'
                         helper_text_mode: "on_focus"
 
                     MDTextField:
                         id: val3
-                        hint_text: "Result"
+                        hint_text: "Resultado"
                         readonly : "True"
                         color_mode: 'custom'
                         icon_right_color: app.theme_cls.primary_color
@@ -63,28 +63,28 @@ ScreenManager:
 
                     MDRoundFlatIconButton:
                         id:add
-                        text: "Addition"
+                        text: "Adição"
                         icon:"calculator"
                         pos_hint: {"center_x": .5, "center_y": .6}
                         on_press: app.addition()
                     
                     MDRoundFlatIconButton:
                         id:sub
-                        text: "Substraction"
+                        text: "Subtração"
                         icon:"calculator"
                         pos_hint: {"center_x": .5, "center_y": .6}
                         on_press: app.sub()
 
                     MDRoundFlatIconButton:
                         id:add
-                        text: "Multification"
+                        text: "Multiplicação"
                         icon:"calculator"
                         pos_hint: {"center_x": .5, "center_y": .6}
                         on_press: app.multi()
 
                     MDRoundFlatIconButton:
                         id:div
-                        text: "Division"
+                        text: "Divisão"
                         icon:"calculator"
                         pos_hint: {"center_x": .5, "center_y": .6}
                         on_press: app.div()
@@ -113,7 +113,7 @@ ScreenManager:
                         
                     MDTextField:
                         id: val5
-                        hint_text: "Result"
+                        hint_text: "Resultado"
                         readonly : "True"
                         color_mode: 'custom'
                         icon_right_color: app.theme_cls.primary_color
@@ -150,68 +150,57 @@ ScreenManager:
                     MDLabel:
                         id: result
     ''')
-        self.title = 'Calculator App'
+        self.title = 'SuperCalc'
         return self.help_string
 
     def tratamentoDeTextoVazio(t,text):
         if not text:
             return 0
-        else:
-            try:
-                return float(text)
-            except:
-                return 0
 
     def addition(self):
-        val1 = self.tratamentoDeTextoVazio(self.help_string.get_screen('Calculator App').ids.val1.text)
-        val2 = self.tratamentoDeTextoVazio(self.help_string.get_screen('Calculator App').ids.val2.text)
+        val1 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val1.text)
+        val2 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val2.text)
         res = val1 + val2
-        self.help_string.get_screen('Calculator App').ids.val3.text = str("{:.5f}".format(res))
+        self.help_string.get_screen('SuperCalc').ids.val3.text = str("{:.5f}".format(res))
 
     def sub(self):
-        val1 = self.tratamentoDeTextoVazio(self.help_string.get_screen('Calculator App').ids.val1.text)
-        val2 = self.tratamentoDeTextoVazio(self.help_string.get_screen('Calculator App').ids.val2.text)
+        val1 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val1.text)
+        val2 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val2.text)
         res = val1 - val2
-        self.help_string.get_screen('Calculator App').ids.val3.text = str("{:.5f}".format(res))
+        self.help_string.get_screen('SuperCalc').ids.val3.text = str("{:.5f}".format(res))
 
     def multi(self):
-        val1 = self.tratamentoDeTextoVazio(self.help_string.get_screen('Calculator App').ids.val1.text)
-        val2 = self.tratamentoDeTextoVazio(self.help_string.get_screen('Calculator App').ids.val2.text)
+        val1 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val1.text)
+        val2 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val2.text)
         res = val1 * val2
-        self.help_string.get_screen('Calculator App').ids.val3.text = str("{:.5f}".format(res))
+        self.help_string.get_screen('SuperCalc').ids.val3.text = str("{:.5f}".format(res))
 
     def div(self):
-        val1 = self.tratamentoDeTextoVazio(self.help_string.get_screen('Calculator App').ids.val1.text)
-        val2 = self.tratamentoDeTextoVazio(self.help_string.get_screen('Calculator App').ids.val2.text)
+        val1 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val1.text)
+        val2 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val2.text)
         if val2:
             res = val1 / val2
-            self.help_string.get_screen('Calculator App').ids.val3.text = str("{:.5f}".format(res))
+            self.help_string.get_screen('SuperCalc').ids.val3.text = str("{:.5f}".format(res))
         else:
-            self.help_string.get_screen('Calculator App').ids.val3.text = "Por favor, insira um valor diferente de zero na segunda entrada"
+            self.help_string.get_screen('SuperCalc').ids.val3.text = "Por favor, insira um valor diferente de zero no segundo valor"
 
     def integration(self):
         x = sp.symbols('x')
-        val4 = self.help_string.get_screen('Calculator App').ids.val4.text
-        if not val4:
-            val4 = 0
+        val4 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val4.text)
         res = sp.integrate(val4, x)
-        self.help_string.get_screen('Calculator App').ids.val5.text = "A integração resulta em " +str(res)
+        self.help_string.get_screen('SuperCalc').ids.val5.text = "A integração resulta em " +str(res)
         
     def derivation(self):
         x = sp.symbols('x')
-        val4 = self.help_string.get_screen('Calculator App').ids.val4.text
-        if not val4:
-            val4 = 0
+        val4 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val4.text)
         res = sp.diff(val4, x)
-        self.help_string.get_screen('Calculator App').ids.val5.text = "A derivação resulta em " +str(res)
+        self.help_string.get_screen('SuperCalc').ids.val5.text = "A derivação resulta em " +str(res)
         
     def Laplace(self):
         t, s = sp.symbols('t s')
-        val4 = self.help_string.get_screen('Calculator App').ids.val4.text
-        if not val4:
-            val4 = 0
+        val4 = self.tratamentoDeTextoVazio(self.help_string.get_screen('SuperCalc').ids.val4.text)
         res = sp.laplace_transform(val4, t, s)
-        self.help_string.get_screen('Calculator App').ids.val5.text = "A transformada é " +str(res)
+        self.help_string.get_screen('SuperCalc').ids.val5.text = "A transformada é " +str(res)
         
 
 if __name__ == '__main__':
